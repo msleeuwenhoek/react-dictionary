f9bd2df1b0a1bd4d453a3e9764803b37de2e908c
msleeuwenhoek <97388771+msleeuwenhoek@users.noreply.github.com>
2022-03-30 22:19:12 +0200

    set default keyword on page render

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function App() {
   return (
     <div className="App">
       <div className="container">
-        <Dictionary />
+        <Dictionary keyword="sunset" />
         <footer>
           <a href="https://github.com/msleeuwenhoek/react-dictionary">
             Open-source code
--- src/Dictionary.js
+++ src/Dictionary.js
@@ -3,32 +3,46 @@ import React, { useState } from "react";
 import "./Dictionary.css";
 import Results from "./Results";
 
-export default function Dictionary() {
-  const [keyword, setKeyword] = useState("");
+export default function Dictionary(props) {
+  const [keyword, setKeyword] = useState(props.keyword);
   const [definition, setDefinition] = useState(null);
+  const [loaded, setLoaded] = useState(false);
 
   function getWordMeaning(response) {
     setDefinition(response.data);
   }
 
-  function Search(event) {
-    event.preventDefault();
+  function Search() {
     const apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en/${keyword}`;
     axios.get(apiUrl).then(getWordMeaning);
+  }
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    Search();
+  }
+
+  function load() {
+    setLoaded(true);
+    Search();
   }
 
   function SetSearchInput(event) {
     event.preventDefault();
     setKeyword(event.target.value);
   }
-
-  return (
-    <div className="Dictionary">
-      <form onSubmit={Search}>
-        <input type="search" onChange={SetSearchInput} />
-        <input type="submit" value="Search" />
-      </form>
-      <Results definition={definition} />{" "}
-    </div>
-  );
+  if (loaded) {
+    return (
+      <div className="Dictionary">
+        <form onSubmit={handleSubmit}>
+          <input type="search" onChange={SetSearchInput} />
+          <input type="submit" value="Search" />
+        </form>
+        <Results definition={definition} />{" "}
+      </div>
+    );
+  } else {
+    load();
+    return null;
+  }
 }
